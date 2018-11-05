16637b851ace98b7632b09a718d033b7aa87544b
Newaz Sharif <newaz.sharif@hotmail.com>
2018-11-05 15:27:24 +0000

    user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
+  validates :username, presence: true, 
+  uniqueness: {case_sensative: false},
+  length: {minimum: 3, maximum: 25}
   
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i 
+  
+  validates :email, 
+  uniqueness: {case_sensative: false},
+  format: { with: VALID_EMAIL_REGEX }
 end
