4418a2ecfecd537904389070f705bed1f5934bbc
Newaz Sharif <newaz.sharif@hotmail.com>
2018-11-30 04:07:04 +0000

    add update feature at category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,7 @@
 class CategoriesController < ApplicationController
   
   before_action :require_admin, :except => [:index,:show]
+  before_action :set_category , only: [:edit,:update,:show,:destroy]
   
   def index
     @categories = Category.paginate(:page => params[:page], :per_page => 5)
@@ -10,6 +11,12 @@ class CategoriesController < ApplicationController
     @category = Category.new
   end
   
+  
+  def show
+    @category = Category.find(params[:id])
+    @category_articles = @category.articles.paginate(:page => params[:page], :per_page => 5)
+  end
+  
   def create
     @category = Category.new(category_params)
     if @category.save
@@ -17,6 +24,22 @@ class CategoriesController < ApplicationController
       redirect_to categories_path
     else
       render 'new'
+    end
+  end
+  
+  def edit
+   
+    
+  end
+  
+  
+  def update
+    
+    if @category.update(category_params)
+      flash[:success] = "Category Updated Successfully"
+      redirect_to edit_category_path(@category)
+    else
+      redirect_to root_path
     end
   end
   
@@ -30,5 +53,9 @@ class CategoriesController < ApplicationController
       flash[:danger] = "only admin can perform this action"
       redirect_to categories_path
     end
+  end
+  
+  def set_category
+     @category = Category.find(params[:id])
   end
 end
